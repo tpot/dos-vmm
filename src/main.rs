12a052114104a30efbc7f17c2176393b94584875
Tim Potter <tpot@frungy.org>
2024-05-10 09:26:52 +1000

    Open /dev/kvm device

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use nix::{
+    fcntl,
+    sys::stat::Mode,
+    fcntl::OFlag,
+};
+
+const KVM_DEVICE: &str = "/dev/kvm";
+
 fn main() {
-    println!("Hello, world!");
+
+    // Open fd /dev/kvm
+    let kvm_fd: i32 = match fcntl::open(KVM_DEVICE, OFlag::O_RDWR, Mode::empty()) {
+        Ok(fd)     => { fd },
+        Err(errno) => { eprintln!("Error opening {KVM_DEVICE}: {errno}");
+                        std::process::exit(1); }
+    };
+
+    println!("fd = {kvm_fd}");
 }
