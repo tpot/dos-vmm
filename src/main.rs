ffae64c82248ebcd428ede005ac15c8c639053a5
Tim Potter <tpot@frungy.org>
2024-05-10 10:56:07 +1000

    Create VM with kvm_create_vm()

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ use nix::{
     fcntl,
     sys::stat::Mode,
     fcntl::OFlag,
+    ioctl_write_int_bad, request_code_none,
+};
+
+use kvm_bindings::{
+    KVMIO,
 };
 
 const KVM_DEVICE: &str = "/dev/kvm";
+
+ioctl_write_int_bad!(kvm_create_vm, request_code_none!(KVMIO, 0x01));
 
 fn main() {
 
@@ -22,4 +29,16 @@ fn main() {
     };
 
     println!("kvm_fd = {0}", AsRawFd::as_raw_fd(&kvm_fd));
+
+    let vm_fd: OwnedFd = match unsafe { kvm_create_vm(kvm_fd.as_raw_fd(), 0) } {
+        Ok(fd) => unsafe {
+            FromRawFd::from_raw_fd(fd)
+        },
+        Err(errno) => {
+            eprintln!("Error in kvm_create_vm: {errno}");
+            std::process::exit(1);
+        },
+    };
+
+    println!("vm_fd = {0}", AsRawFd::as_raw_fd(&vm_fd));
 }
