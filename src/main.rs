a6f10f976785e9fb82c1eecf9715dd0cbde973dd
Tim Potter <tpot@frungy.org>
2024-05-10 10:24:12 +1000

    Convert /dev/kvm fd to std::os::fd:OwnedFd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::os::fd::{AsRawFd, FromRawFd, OwnedFd};
+
 use nix::{
     fcntl,
     sys::stat::Mode,
@@ -8,12 +10,16 @@ const KVM_DEVICE: &str = "/dev/kvm";
 
 fn main() {
 
-    // Open fd /dev/kvm
-    let kvm_fd: i32 = match fcntl::open(KVM_DEVICE, OFlag::O_RDWR, Mode::empty()) {
-        Ok(fd)     => { fd },
-        Err(errno) => { eprintln!("Error opening {KVM_DEVICE}: {errno}");
-                        std::process::exit(1); }
+    // Open /dev/kvm
+    let kvm_fd: OwnedFd = match fcntl::open(KVM_DEVICE, OFlag::O_RDWR, Mode::empty()) {
+        Ok(fd) => unsafe {
+            FromRawFd::from_raw_fd(fd)
+        },
+        Err(errno) => {
+            eprintln!("Error opening {KVM_DEVICE}: {errno}");
+            std::process::exit(1);
+        },
     };
 
-    println!("fd = {kvm_fd}");
+    println!("kvm_fd = {0}", AsRawFd::as_raw_fd(&kvm_fd));
 }
